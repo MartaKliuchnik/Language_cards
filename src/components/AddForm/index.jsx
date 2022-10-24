23d1fd53828cff0566bf3eb12690482259b37d4b
Marta <martasagun95@gmail.com>
2022-10-24 15:39:28 +0200

    added function change language in all cards

--- src/components/AddForm/index.jsx
+++ src/components/AddForm/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './index.module.sass';
 import { useDispatch } from 'react-redux';
 import { addCard } from '../../store/reducer/languageReducer';
+import ChangeLanguage from '../ChangeLanguage';
 
 export default function AddForm() {
 
@@ -17,6 +18,7 @@ export default function AddForm() {
     return (
         <form onSubmit={submit}
             className={['wrapper', s.add_form].join(' ')}>
+            <ChangeLanguage/>
             <div className={s.input_container}>
                 <input type="text" name="ru" placeholder='Russian word' />
                 <input type="text" name="en" placeholder='English word' />
